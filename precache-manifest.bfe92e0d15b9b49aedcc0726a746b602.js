d24ea865b395a597d6d172b9b1850b433d5667fb
Kuldeep K Srivastava <kk23907.me@gmail.com>
2020-09-27 21:49:29 +0530

    Updates

---
+++ precache-manifest.bfe92e0d15b9b49aedcc0726a746b602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343b67d23fd54abd7ffa977aba4c2ae3",
+    "url": "/Bifrost/index.html"
+  },
+  {
+    "revision": "89d89853c3da9e299cab",
+    "url": "/Bifrost/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1185f1324fb8db687bb6",
+    "url": "/Bifrost/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89d89853c3da9e299cab",
+    "url": "/Bifrost/static/js/2.8b5b41b2.chunk.js"
+  },
+  {
+    "revision": "8d4d3a81fd74d2d657b02a87db5fe0ed",
+    "url": "/Bifrost/static/js/2.8b5b41b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1185f1324fb8db687bb6",
+    "url": "/Bifrost/static/js/main.6afdd19a.chunk.js"
+  },
+  {
+    "revision": "16d264c80dbac94220a3",
+    "url": "/Bifrost/static/js/runtime-main.add751a6.js"
+  }
+]);
